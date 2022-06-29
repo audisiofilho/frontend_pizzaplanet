fc5310a3400a9cbc37955d29ac30e98633322e49
audisiofilho <obuzua46@gmail.com>
2022-06-28 22:01:10 -0300

    Finalizando tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home() {
               Acessar
             </Button>
           </form>
+          <a className={styles.text}>Não Possui uma conta? Cadastre-se</a>
         </div>
       </div>
     </>
