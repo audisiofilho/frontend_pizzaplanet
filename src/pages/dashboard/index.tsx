7a7b3252afc92a63f5742933584d4b661cfbb525
audisiofilho <obuzua46@gmail.com>
2022-07-07 21:48:51 -0300

    Criando tela dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,19 +1,38 @@
 import Head from "next/head";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 
+import styles from "./styles.module.scss";
+
 import { Header } from "../../components/Header";
+import { FiRefreshCcw } from "react-icons/fi";
 
 export default function Dashboard() {
   return (
-  <>
-  <Head>
-    <title>Painel - PizzaPlanet</title>
-  </Head>
-  <div>
-    <Header/>
-    <h1>painel</h1>
-  </div>
-  </>
+    <>
+      <Head>
+        <title>Painel - PizzaPlanet</title>
+      </Head>
+      <div>
+        <Header />
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Ultimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa 34</span>
+              </button>
+            </section>
+          </article>
+        </main>
+      </div>
+    </>
   );
 }
 
